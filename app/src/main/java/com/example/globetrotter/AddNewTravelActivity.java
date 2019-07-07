12c9c794ca73330369a8c2c1b3094653a424fae8
Helentsai <pupu.her@hotmail.com>
2019-07-07 20:40:36 +0800

    add firebase auth and addNewCountry page

--- app/src/main/java/com/example/globetrotter/AddNewTravelActivity.java
+++ app/src/main/java/com/example/globetrotter/AddNewTravelActivity.java
@@ -4,11 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddNewTravelActivity extends AppCompatActivity {
+
+    FirebaseApp mApp;
+    FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_travel);
+        initFirebase();
+        readData();
+    }
+
+
+
+    private void initFirebase() {
+        mApp = FirebaseApp.getInstance();
+        mDatabase = FirebaseDatabase.getInstance(mApp);
+
+    }
+
+    private void readData() {
+    }
+
+    private void writeData(){
+        DatabaseReference ref = mDatabase.getReference("travelD").child("datetime");
+
     }
 }
