5209c349ec673d1798ba2cad86a649e02d341642
shethsukhada <45674029+shethsukhada@users.noreply.github.com>
2019-07-10 07:13:05 -0400

    adding spinner for country selection

--- app/src/main/java/com/example/globetrotter/AddNewTravelActivity.java
+++ app/src/main/java/com/example/globetrotter/AddNewTravelActivity.java
@@ -2,15 +2,20 @@ package com.example.globetrotter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AddNewTravelActivity extends AppCompatActivity {
+public class AddNewTravelActivity extends AppCompatActivity implements View.OnClickListener {
 
     FirebaseApp mApp;
     FirebaseDatabase mDatabase;
@@ -20,14 +25,62 @@ public class AddNewTravelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_travel);
 
+        // Buttons sukhada
+        findViewById(R.id.asia).setOnClickListener(this);
+        findViewById(R.id.africa).setOnClickListener(this);
+        findViewById(R.id.northAmerica).setOnClickListener(this);
+        findViewById(R.id.southAmerica).setOnClickListener(this);
+        findViewById(R.id.europe).setOnClickListener(this);
+
+        addListenerOnSpinnerItemSelection();
+
     }
 
-    public void clickList(View view){
-        Intent intent = new Intent(this,checkListActivity.class);
+    public void clickList(View view) {
+        Intent intent = new Intent(this, checkListActivity.class);
         startActivity(intent);
     }
 
+    @Override
+    public void onClick(View v) {
+        Spinner spinner = (Spinner) findViewById(R.id.country_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.europe_array, android.R.layout.simple_spinner_item);
+
+        int i = v.getId();
+        if (i == R.id.europe) {
+// Create an ArrayAdapter using the string array and a default spinner layout
+            adapter = ArrayAdapter.createFromResource(this,
+                    R.array.europe_array, android.R.layout.simple_spinner_item);
+        } else if (i == R.id.asia) {
+            // Create an ArrayAdapter using the string array and a default spinner layout
+            adapter = ArrayAdapter.createFromResource(this,
+                    R.array.asia_array, android.R.layout.simple_spinner_item);
+        } else if (i == R.id.southAmerica) {
+// Create an ArrayAdapter using the string array and a default spinner layout
+            adapter = ArrayAdapter.createFromResource(this,
+                    R.array.south_america_array, android.R.layout.simple_spinner_item);
+        } else if (i == R.id.northAmerica) {
+// Create an ArrayAdapter using the string array and a default spinner layout
+            adapter = ArrayAdapter.createFromResource(this,
+                    R.array.north_america_array, android.R.layout.simple_spinner_item);
+        } else if (i == R.id.africa) {
+// Create an ArrayAdapter using the string array and a default spinner layout
+            adapter = ArrayAdapter.createFromResource(this,
+                    R.array.africa_array, android.R.layout.simple_spinner_item);
+        }
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
 
 
-
+    public void addListenerOnSpinnerItemSelection() {
+        Spinner spinner1 = (Spinner) findViewById(R.id.country_spinner);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
 }
+
+
