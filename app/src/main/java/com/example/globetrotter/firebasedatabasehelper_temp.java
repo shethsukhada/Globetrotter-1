6befa064db9af8d867651ad9052cdf53839bdc13
shethsukhada <45674029+shethsukhada@users.noreply.github.com>
2019-07-11 00:43:40 -0400

    firebase write ..and some images and some fixes

---
+++ app/src/main/java/com/example/globetrotter/firebasedatabasehelper_temp.java
@@ -0,0 +1,74 @@
+package com.example.globetrotter;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.globetrotter.Travel;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class firebasedatabasehelper_temp {
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReferenceTravel;
+    private List<Travel> travel =  new ArrayList<>();
+
+
+    public firebasedatabasehelper_temp(){
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mReferenceTravel = mDatabase.getReference("globletrotter");
+        Log.d("adadsa","object creation");
+
+
+    }
+
+    public interface DataStatus{
+        void DataIsLoaded(List<Travel> travel, List<String> keys);
+        void DataIsInserted();
+        void DataIsUpdated();
+        void DataIsDeleted();
+    }
+
+    public void read_travel(final DataStatus dataStatus) {
+        mReferenceTravel.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                travel.clear();
+                List<String> keys = new ArrayList<>();
+                for(DataSnapshot keyNode : dataSnapshot.getChildren()){
+                    keys.add(keyNode.getKey());
+                    Travel travel_l = keyNode.getValue(Travel.class);
+                    travel.add(travel_l);
+                }
+                dataStatus.DataIsLoaded(travel,keys);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void add_tarvel_fb(Travel travel, final DataStatus dataStatus ){
+        String key = mReferenceTravel.push().getKey();
+        mReferenceTravel.child(key).setValue(travel)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        dataStatus.DataIsInserted();
+                    }
+                });
+     Log.d("adadsa" , key);
+    }
+}
